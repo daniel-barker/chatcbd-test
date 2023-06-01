be9afe6614996d650caa0985f095bda626c84b8f
dan barker <dbarker16@gmail.com>
2023-06-01 12:48:38 -0400

    installed app, made a model for shoes

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 Team:
 
 * Person 1 - Which microservice?
+Daniel Barker - Shoes
 * Person 2 - Which microservice?
+Chris Marquand - Hats
 
 ## Design
 
 ## Shoes microservice
 
-Explain your models and integration with the wardrobe
-microservice, here.
+currently whiteknuckling, will get back to you soon.
 
 ## Hats microservice
 
--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class BinVO(models.Model):
+
+    idkyet = models.CharField
+
+class Shoe(models.Model):
+
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture = models.URLField()
+
+    bin = models.ForeignKey(
+        BinVO,
+        related_name="shoes",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_api_url(self):
+        return reverse("api_show_attendee", kwargs={"pk": self.pk})

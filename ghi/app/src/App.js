58a5b264e5c5fda88a4c4e3fd5c1ddb7890882da
Chris <megaquand@gmail.com>
2023-06-02 08:15:03 -0400

    adds HatList.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import HatList from './HatList';
+
 
 function App() {
+  if (props.hats === undefined) {
+    return null;
+  }
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
+        <HatList hats={props.hats} />
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>

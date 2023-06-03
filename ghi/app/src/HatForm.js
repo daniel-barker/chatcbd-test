f8d79ec0d225eea88c9066dc23720c0cbd2ebec4
Chris <megaquand@gmail.com>
2023-06-02 21:47:37 -0400

    made changes to HatsForm

--- ghi/app/src/HatForm.js
+++ ghi/app/src/HatForm.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 function HatForm () {
-  const [states, setStates] = useState([]);
   const [fabric, setFabric] = useState('');
   const [styleName, setStyleName] = useState('');
   const [color, setColor] = useState('');
+  const [hatPicture, setPictureUrl] = useState('')
+  const [location, setLocation] = useState('')
+    const [locations, setLocations] = useState([])
 
   const handleFabricChange = (event) => {
     const value = event.target.value;
@@ -18,76 +20,80 @@ function HatForm () {
     const value = event.target.value;
     setColor(value);
   }
+  const handlePictureChange = (event) => {
+    const value = event.target.value
+    setPictureUrl(value)
+  }
+  const handleLocationChange = (event) => {
+    const value = event.target.value
+    setLocation(value)
+  }
+  const fetchData = async () => {
+    const bin = 'http://localhost:8090/api/locations/'
+    const response = await fetch(location)
+    if (response.ok) {
+        const data = await response.json()
+        setLocations(data.locations)
+    }
+}
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = {};
-
     data.fabric = fabric;
     data.style_name = styleName;
     data.color = color;
 
     const hatUrl = 'http://localhost:8090/api/hats/';
     const fetchConfig = {
-    method: "post",
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+  }
   const response = await fetch(hatUrl, fetchConfig);
   if (response.ok) {
-    const newHat = await response.json();
-    console.log(newHat);
+    const newLocation = await response.json();
+    console.log(newLocation);
 
     setFabric('');
     setStyleName('');
     setColor('');
   }
-  const fetchData = async () => {
-    const url = 'http://localhost:8000/api/hats/';
-
-    const response = await fetch(url);
-
-    if (response.ok) {
-      const data = await response.json();
-      }
-
-    }
-  }
+}
 
   useEffect(() => {
-    // fetchData();
+      fetchData();
   }, []);
 
     return (
         <div className="row">
         <div className="offset-3 col-6">
           <div className="shadow p-4 mt-4">
-            <h1>Add a new hat</h1>
-            <form onSubmit={handleSubmit} id="create-hat-form">
+            <h1>Add a new Hat to your Wardrobe!</h1>
+            <form onSubmit={handleSubmit} id="create-location-form">
               <div className="form-floating mb-3">
-                <input onChange={handleFabricChange} placeholder="Fabric" required type="text" name="fabric" id="fabric" className="form-control" value={fabric}/>
+                <input onChange={handleFabricChange} value={fabric} placeholder={"Fabric"} required type="text" name="fabric" id="fabric" className="form-control"/>
                 <label htmlFor="name">Fabric</label>
               </div>
               <div className="form-floating mb-3">
-                <input onChange={handleStyleNameChange} placeholder="Style name" required type="number" name="style_name" id="style_name" className="form-control" value={styleName}/>
+                <input onChange={handleStyleNameChange} value={styleName} placeholder="Style name" required type="number" name="style_name" id="style_name" className="form-control"/>
                 <label htmlFor="room_count">Style name</label>
               </div>
               <div className="form-floating mb-3">
-                <input onChange={handleColorChange} placeholder="Color" required type="text" name="color" id="color" className="form-control" value={color}/>
+                <input onChange={handleColorChange} value={color} placeholder="Color" required type="text" name="color" id="color" className="form-control"/>
                 <label htmlFor="color">Color</label>
               </div>
               <div className="mb-3">
-                <select required name="hat" id="hat" className="form-select">
-                  <option selected value="">Choose a hat</option>
-                  {/* need some work here */}
-                  {states.map(hat => {
-                    return (
-                      <option key={hat.href} value={hat.href}>
-                        {hat.closet_name}
-                      </option>
-                    );
-                  })}
+                  <select onChange={handleLocationChange} value={location} required id="locations" name="locations" className="form-select">
+                      <option value="">Choose a location</option>
+                      {locations.map(location => {
+                          return (
+                            <option key={location.id} value={location.href}>
+                                {location.closet_name}
+                            </option>
+                        )
+                    })}
                 </select>
               </div>
               <button className="btn btn-primary">Create</button>
@@ -96,6 +102,6 @@ function HatForm () {
         </div>
       </div>
     );
-}
+  }
 
 export default HatForm;
